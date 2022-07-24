886f28e584c18ecd9c67a8acca38c0a73dd53ced
ElLimitador <80768621+NicoJGutierrez@users.noreply.github.com>
2022-07-24 15:48:47 -0400

    Agregado sistema de grupo zombies
    Ahora las balas detectan los zombies y ellos printean "hit"

--- Bullet.gd
+++ Bullet.gd
@@ -8,7 +8,6 @@ export var laiftaim = 2
 
 var velocity = Vector3.ZERO
 
-
 func _physics_process(delta):
 	laiftaim -= delta
 	velocity += g * delta
@@ -21,7 +20,7 @@ func _physics_process(delta):
 
 func _on_Bullet_body_entered(body):
 	#print("bum")
-	if body.name == "Zombie":
+	if body.is_in_group("Zombies"):
 		body.bullet_hit() 
 	emit_signal("exploded", transform.origin)
 	queue_free()
