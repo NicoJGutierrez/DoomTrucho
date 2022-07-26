4ce3e117e5ec9c6c71e99299d8b3f2d50be780ff
ElLimitador <80768621+NicoJGutierrez@users.noreply.github.com>
2022-07-26 00:41:57 -0400

    Lifebar con colores
    amarillo = tocado
    rojo = menos de la mitad de la vida

--- VidaDeZombie.gd
+++ VidaDeZombie.gd
@@ -2,6 +2,10 @@ extends Sprite3D
 
 onready var barra = $Viewport/Lifebar
 
+export var green = preload("res://lifebar_textures/barHorizontal_green.png")
+export var yellow = preload("res://lifebar_textures/barHorizontal_yellow.png")
+export var red = preload("res://lifebar_textures/barHorizontal_red.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	texture = $Viewport.get_texture()
@@ -11,3 +15,7 @@ func _ready():
 func update(health, max_health):
 	barra.max_value = max_health
 	barra.value = health
+	if health <= max_health/2:
+		barra.texture_progress = red
+	else:
+		barra.texture_progress = yellow
--- Zombie.gd
+++ Zombie.gd
@@ -2,7 +2,7 @@ extends KinematicBody
 
 
 # Declare member variables here. Examp
-export var max_health = 2
+export var max_health = 3
 var health
 
 # Called when the node enters the scene tree for the first time.
