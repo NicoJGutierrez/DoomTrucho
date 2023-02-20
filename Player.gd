3eb4a3a4a1e434edafb97600f0dff3e89ac98875
ElLimitador <80768621+NicoJGutierrez@users.noreply.github.com>
2023-02-20 18:28:10 -0300

    feat: splitscreen adaptation

--- Player.gd
+++ Player.gd
@@ -8,44 +8,57 @@ export (PackedScene) var Bullet
 var gravity = -65
 var on_floor_margin = 1
 var max_speed = 8
-var mouse_sensitivity = 0.002  # radians/pixel
 var jumps = 2
 var jumpforce = 20
+
+export var player_number = "1"
+var player_number_int = int(player_number)
+
+var camera_sensitivity = 2
 
 var velocity = Vector3()
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$Cuerpo.set_layer_mask_bit(1+player_number_int, true)
+	$Cuerpo.set_layer_mask_bit(1, false)
+	camera.set_cull_mask_bit(1+player_number_int, false)
 
 func get_input():
 	var input_dir = Vector3()
 	# desired move in camera direction
-	if Input.is_action_pressed("move_forward"):
+	if Input.is_action_pressed("p"+player_number+"f"):
 		input_dir += -camera.global_transform.basis.z
-	if Input.is_action_pressed("move_back"):
+	if Input.is_action_pressed("p"+player_number+"b"):
 		input_dir += camera.global_transform.basis.z
-	if Input.is_action_pressed("strafe_left"):
+	if Input.is_action_pressed("p"+player_number+"l"):
 		input_dir += -camera.global_transform.basis.x
-	if Input.is_action_pressed("strafe_right"):
+	if Input.is_action_pressed("p"+player_number+"r"):
 		input_dir += camera.global_transform.basis.x
 	input_dir = input_dir.normalized()
 	return input_dir
 	
-func _unhandled_input(event):
-	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotation_helper.rotate_x(-event.relative.y * mouse_sensitivity)
-		rotate_y(-event.relative.x * mouse_sensitivity)
-		rotation_helper.rotation.x = clamp(rotation_helper.rotation.x, -1.2, 1.2)
-		
+func get_camera_input(delta):
+	if Input.is_action_pressed("p"+player_number+"lu"):
+		rotation_helper.rotate_x(delta * camera_sensitivity)
+	if Input.is_action_pressed("p"+player_number+"ld"):
+		rotation_helper.rotate_x(-delta * camera_sensitivity)
+	if Input.is_action_pressed("p"+player_number+"ll"):
+		rotate_y(delta * camera_sensitivity)
+	if Input.is_action_pressed("p"+player_number+"lr"):
+		rotate_y(-delta * camera_sensitivity)
+	rotation_helper.rotation.x = clamp(rotation_helper.rotation.x, -1.2, 1.2)
+	
+	
 func _physics_process(delta):
 	
 	var desired_velocity = get_input() * max_speed
+	var camera_movement = get_camera_input(delta)# * camera_sensitivity
 
 	velocity.x = desired_velocity.x
 	velocity.z = desired_velocity.z
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 	
-	if Input.is_action_just_pressed("jump") and jumps > 0:
+	if Input.is_action_just_pressed("p"+player_number+"j") and jumps > 0:
 			velocity.y = jumpforce
 			jumps -= 1
 	
@@ -60,7 +73,7 @@ func _physics_process(delta):
 		
 	
 	#disparo
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("p"+player_number+"s"):
 		var b = Bullet.instance()
 		owner.add_child(b)
 		b.transform = $Cabeza/Chutspot.global_transform
--- Zombie.gd
+++ Zombie.gd
@@ -6,30 +6,25 @@ export var max_health = 3
 var health
 
 onready var nav: Navigation = $"../Navigation"
-onready var player: KinematicBody = $"../Player"
+#onready var player: KinematicBody = $"../Player"
+var target: KinematicBody = null
 var path: Array = []
 export var speed: int = 4
 var current_node = 0
+var margin = .1
 
 var velocity = Vector3()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_path(player.global_transform.origin)
+#	update_path(player.global_transform.origin)
 	health = max_health
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if current_node <= path.size() and path != [] and current_node < path.size():
-		var direction: Vector3 = path[current_node] - global_transform.origin
-		if direction.length() <= 1:
-			current_node += 1
-		else:
-			velocity = direction.normalized() * speed #* delta
-			velocity = move_and_slide(velocity, Vector3.UP, true)
-	else:
-		update_path(player.global_transform.origin)
-		current_node = 0
+	if path.size() > 0:
+		move_to_target(target)
+
 
 func bullet_hit():
 	#print("hit")
@@ -38,11 +33,21 @@ func bullet_hit():
 	if health == 0:
 		queue_free()
 
-func update_path(target):
-	path = nav.get_simple_path(global_transform.origin, target)
-	#print(path)
+func move_to_target(target):
+	if current_node >= path.size():
+		return
+	elif global_transform.origin.distance_to(path[current_node]) < margin:
+		current_node += 1
+	else:
+		var direction: Vector3 = path[current_node] - global_transform.origin
+		velocity = direction.normalized() * speed
+		move_and_slide(velocity, Vector3.UP)
+		pass
+		
+	#path = nav.get_simple_path(global_transform.origin, target)
+#	#print(path)
 
 
-func _on_Timer_timeout():
-	update_path(player.global_transform.origin)
-	current_node = 0
+#func _on_Timer_timeout():
+#	update_path(player.global_transform.origin)
+#	current_node = 0
