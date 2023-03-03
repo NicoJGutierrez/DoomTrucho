0649b2a65b32461ae497d3434384af949b44a3e1
ElLimitador <80768621+NicoJGutierrez@users.noreply.github.com>
2023-03-03 17:04:06 -0300

    feat: crouch hoping

--- Player.gd
+++ Player.gd
@@ -5,12 +5,17 @@ onready var rotation_helper = $Cabeza
 
 export (PackedScene) var Bullet
 
-var gravity = -65
+export var gravity = -50
+var applied_gravity = gravity
 var on_floor_margin = 1
 var max_speed = 8
+var slide_speed = 10
 var velocity = Vector3()
-var jumps = 2
-var jumpforce = 20
+var stored_velocity = Vector3()
+export var max_jumps = 1
+var jumps = max_jumps
+var jumpforce = 35
+var in_air = false
 
 export var max_health = 3
 var health
@@ -29,6 +34,62 @@ func _ready():
 	
 	#vida inicial
 	health = max_health
+
+
+func _physics_process(delta):
+	
+	if in_air == true and is_on_floor():
+		$Bhop.start()
+		in_air = false
+	
+	var desired_velocity = get_input() * max_speed
+	if not Input.is_action_pressed("p"+player_number+"sl"):
+		# movimiento fome
+		if is_on_floor() and $Bhop.is_stopped():
+			velocity.x = desired_velocity.x
+			velocity.z = desired_velocity.z
+			applied_gravity = gravity
+		# movimiento aéreo
+		else:
+			velocity.x += desired_velocity.x * 0.05
+			velocity.z += desired_velocity.z * 0.05
+			
+	
+	#jump + doublejump
+	if Input.is_action_just_pressed("p"+player_number+"j") and jumps > 0:
+			in_air = true
+			velocity.y = jumpforce * 0.75
+			jumps -= 1
+			velocity.x = desired_velocity.x
+			velocity.z = desired_velocity.z
+	elif is_on_floor():
+		jumps = max_jumps
+		velocity.y = -on_floor_margin * delta
+	else:
+		velocity.y += applied_gravity * delta
+	
+	#Slide
+	if Input.is_action_just_pressed("p"+player_number+"sl"):
+		$AnimationPlayer.play("Slide")
+		if is_on_floor():
+			var slide = slide_speed * get_input()
+			velocity += Vector3(slide.x, 0, slide.z)
+		else:
+			velocity += Vector3(0, -slide_speed, 0)
+			applied_gravity = gravity * 3
+	if Input.is_action_just_released("p"+player_number+"sl"):
+		$AnimationPlayer.play("RESET")
+	
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+	var _camera_movement = get_camera_input(delta, velocity.length())
+	
+	#disparo
+	if Input.is_action_pressed("p"+player_number+"s"):
+		shoot(Bullet)
+		$Cabeza/Chutspot/Particles.emitting = true
+	else:
+		$Cabeza/Chutspot/Particles.emitting = false
 
 func get_input():
 	var input_dir = Vector3()
@@ -52,49 +113,16 @@ func get_camera_input(delta, player_speed):
 	var look_left_right = Input.get_axis("p"+player_number+"lr", "p"+player_number+"ll")
 	rotate_y(delta * movement * look_left_right)
 	rotation_helper.rotation.x = clamp(rotation_helper.rotation.x, -1.5, 1.5)
-	
-	
-func _physics_process(delta):
-	
-	var desired_velocity = get_input() * max_speed
 
-	velocity.x = desired_velocity.x
-	velocity.z = desired_velocity.z
-	
-	if Input.is_action_just_pressed("p"+player_number+"j") and jumps > 0:
-			velocity.y = jumpforce
-			jumps -= 1
-	
-	elif is_on_floor():
-		#print("floor")
-		jumps = 2
-		velocity.y = -on_floor_margin * delta
-		
-	else:
-		#print("air")
-		velocity.y += gravity * delta
-	
-	if Input.is_action_just_pressed("p"+player_number+"sl"):
-		$AnimationPlayer.play("Slide")
-	if Input.is_action_just_released("p"+player_number+"sl"):
-		$AnimationPlayer.play("RESET")
-	
-	velocity = move_and_slide(velocity, Vector3.UP, true)
-	
-	var _camera_movement = get_camera_input(delta, velocity.length())
-	
-	#disparo
-	if Input.is_action_pressed("p"+player_number+"s"):
-		if $FireRate.is_stopped():
-			var b = Bullet.instance()
+func shoot(projectile):
+	if $FireRate.is_stopped():
+			var b = projectile.instance()
 			get_node('/root/Escena1').add_child(b)
 			b.transform = $Cabeza/Chutspot.global_transform
 			b.velocity = -b.transform.basis.z * b.muzzle_velocity
 			b.shooter = player_number
 			$FireRate.start()
-		$Cabeza/Chutspot/Particles.emitting = true
-	else:
-		$Cabeza/Chutspot/Particles.emitting = false
+
 func bullet_hit():
 	#print("hit")
 	health -= 1
